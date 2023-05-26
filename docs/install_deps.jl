78dcf2c293499b1652acc9fb27c37f8b31b8cc25
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2023-05-26 17:11:38 +0200

    docs develop before add?

--- docs/install_deps.jl
+++ docs/install_deps.jl
@@ -1,8 +1,8 @@
 using Pkg
 
-Pkg.add(;url="https://github.com/manuelbb-upb/MultiTaskLearning.jl.git"); 
-Pkg.add(;url="https://github.com/manuelbb-upb/MultiMLDatasets.jl.git")
-
 Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
+
+Pkg.add(;url="https://github.com/manuelbb-upb/MultiTaskLearning.jl.git"); 
+Pkg.add(;url="https://github.com/manuelbb-upb/MultiMLDatasets.jl.git");
 
 Pkg.instantiate()
