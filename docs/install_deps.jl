f75bd4d521043cc010d69d3d6f71d9834f14adc4
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2023-05-26 17:08:56 +0200

    path for packages spec

--- docs/install_deps.jl
+++ docs/install_deps.jl
@@ -3,6 +3,6 @@ using Pkg
 Pkg.add(;url="https://github.com/manuelbb-upb/MultiTaskLearning.jl.git"); 
 Pkg.add(;url="https://github.com/manuelbb-upb/MultiMLDatasets.jl.git")
 
-Pkg.develop(PackageSpec(path=pwd()))
+Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
 
 Pkg.instantiate()
