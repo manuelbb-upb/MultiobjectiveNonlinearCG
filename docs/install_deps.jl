55b5620ac1c2def7cd2085d43a158eec816eb080
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2023-05-26 17:22:14 +0200

    reintroduce manifest toml for docs

--- docs/install_deps.jl
+++ docs/install_deps.jl
@@ -1,7 +1,11 @@
 using Pkg
 
-Pkg.add(;url="https://github.com/manuelbb-upb/MultiMLDatasets.jl.git");
-Pkg.add(;url="https://github.com/manuelbb-upb/MultiTaskLearning.jl.git"); 
+#=
+# This does not work without a custom registry -- I am too lazy for that and 
+# upload the Manifest.toml instead...
+Pkg.develop(;url="https://github.com/manuelbb-upb/MultiMLDatasets.jl.git");
+Pkg.develop(;url="https://github.com/manuelbb-upb/MultiTaskLearning.jl.git"); 
+=#
 
 Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
 Pkg.instantiate()
