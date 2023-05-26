69022aea67f6242b3dc81b1e7f2398835a009910
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2023-05-26 17:06:20 +0200

    order of docs deps

--- docs/install_deps.jl
+++ docs/install_deps.jl
@@ -1,6 +1,8 @@
 using Pkg
 
-Pkg.add(;url="https://github.com/manuelbb-upb/MultiTaskLearning.jl.git"); Pkg.develop(PackageSpec(path=pwd()))
+Pkg.add(;url="https://github.com/manuelbb-upb/MultiTaskLearning.jl.git"); 
 Pkg.add(;url="https://github.com/manuelbb-upb/MultiMLDatasets.jl.git")
+
+Pkg.develop(PackageSpec(path=pwd()))
 
 Pkg.instantiate()
