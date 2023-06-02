ce85b18d16e495162d3c68cbefaeb0698d219aed
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2023-06-02 19:09:34 +0200

    fix PRP3 loops

--- src/dir_rules/prp.jl
+++ src/dir_rules/prp.jl
@@ -23,8 +23,7 @@ struct PRPCache{N, C, S, D, FW} <: AbstractDirCache
     criticality :: C        # Ref to an appropriate criticality measure
     stepsize_cache :: S     # cache for the stepsize calculation
     d_prev :: D             # previous direction vector
-    dj :: D                 # temporary direction vector
-    dtmp :: D               # temporary direction vector
+    dl :: D                 # temporary direction vector
     y :: D                  # working array for δₖ₋₁ or δₖ₋₁ - δₖ
     criticality_measure :: Symbol
     fw_cache :: FW
@@ -48,7 +47,6 @@ function init_cache(descent_rule::PRP, x, fx, DfxT, d, objf!, jacT!, objf_and_ja
         Ref(typemax(T)),
         Ref(typemax(T)),
         stepsize_cache,
-        copy(d),
         copy(d),
         copy(d),
         copy(d), 
@@ -92,53 +90,93 @@ function step!(dc::PRPCache, d, x, fx, DfxT, objf!, jacT!, meta)
 
     # unpack temporary arrays
     d_ = dc.d_prev
-    dj = dc.dj
-    dtmp = dc.dtmp
+    dl = dc.dl
 
     K = meta.dim_out
 
-    #=
-    ψ(ℓ,j) = let gℓ=DfxT[:,ℓ], gj=DfxT[:, j];
-        β = gj'y / ω
-        θ = gj'd_ / ω
-        dd = @. d + β * d_ - θ * y
-        gℓ'dd
-    end
-    Φ = zeros(K,K)
-    for ℓ=1:K
-        for j=1:K
-            Φ[ℓ, j] = ψ(ℓ, j)
-        end
-    end
-    display(Φ)
-     maximum(minimum(Φ, dims=2))
-     minimum(maximum(Φ, dims=1))
+    #= 
+    As per our notes, we want to solve
+    ```math
+    \max_l \min_j ⟨ gₗ, δₖ + β(j) dₖ₋₁ + θ(j) yₖ ⟩,
+    β(j) = ⟨ gⱼ, yₖ ⟩, θ(j) = ⟨ gⱼ, dₖ₋₁ ⟩ 
+    ```
+    Look at the function
+    ```math
+    \begin{algined}
+        Ψ(l, j)     &= ⟨ gₗ, δₖ + β(j) dₖ₋₁ - θ(j) yₖ ⟩             \\
+                    &= ⟨gₗ,δₖ⟩ + ⟨gⱼ,yₖ⟩⟨gₗ,dₖ₋₁⟩ - ⟨gⱼ,dₖ₋₁⟩⟨gₗ,yₖ⟩        \\
+                    &= ⟨gₗ,δₖ⟩ + ⟨gⱼ,yₖ⟩⟨gₗ,dₖ₋₁⟩ - ⟨gⱼ,dₖ₋₁⟩⟨gₗ,yₖ⟩        \\
+                    &= ⟨gₗ,δₖ⟩ + ⟨gⱼ,yₖ⟩θ(l) - ⟨gⱼ,dₖ₋₁⟩β(l)            \\
+                    &= ⟨gₗ,δₖ⟩ - ( -⟨gⱼ,yₖ⟩θ(l) + ⟨gⱼ,dₖ₋₁⟩β(l) + ⟨gⱼ,δₖ⟩ ) + ⟨gⱼ,δₖ⟩   \\
+                    &= ⟨gₗ,δₖ⟩ - Ψ(j, l) + ⟨gⱼ,δₖ⟩                   \\
+                    &= -Ψ(j, l) + ⟨gₗ + gⱼ, δₖ⟩                     \\
+                    &= -Ψ(j,l) + r(l,j).
+    \end{aligned}
+    ```
+    ``r(l,j)`` is symmetric and ``r(l,j) = r(j,l) < 0`` for all ``j`` and ``l``.
+    Hence,
+    ```math
+    \max_l \min_j Ψ(l,j)    = \max_l \min_j -Ψ(j,l) + r(j,l)
+                            = \max_l -\max_j Ψ(j,l) - r(j,l)
+                            = -\min_l \max_j Ψ(j,l) - r(j,l)
+    ```
+    Any of the expressions on the RHS can be used to determine optimal ``(l,j)``.
+    Essentially, it boils down to a memory-computation tradeoff:
+    In the LHS, for each gradient ``gₗ`` we would have to test potenntial directions ``dⱼ``.
+    So it makes sense to precompute all ``dⱼ`` an store them in a matrix with ``nk`` entries.
+    In the RHS, we fix the direction ``dₗ`` and test against all gradients, which are stored, 
+    anyways.
+    
+    We can save even more computations by taking one of the earlier equalities:
+    ```math
+    \max_l \min_j Ψ(l,j) = \max_l \min_j ⟨gₗ,δₖ⟩ + ⟨gⱼ,yₖ⟩θ(l) - ⟨gⱼ,dₖ₋₁⟩β(l)
+                         = -\min_l \max_j ⟨gⱼ,dₖ₋₁⟩β(l) - ⟨gⱼ,yₖ⟩θ(l) - ⟨gₗ,δₖ⟩
+                         = -\min_l -⟨gₗ,δₖ⟩ + \max_j ⟨gⱼ, β(l)dₖ₋₁ - θ(l)yₖ⟩.
+    ```
+    And thats what we do below.
+    We only have to keep in mind, that the optimal MiniMax solution of the RHS 
+    ``δ + β(l)dₖ₋₁ - θ(l)yₖ`` is not the optimal MaxiMin solution of the LHS,
+    so we also have store the optimal `j_opt` whenever the MiniMax changes 
+    and (re-)compute β(j) and θ(j) afterwards.
     =#
-    φmax = typemin(T)
-    for j=1:K
-        gj = DfxT[:, j]
-        β = gj'y / ω
-        θ = gj'd_ / ω
-        @. dj = d + β * d_ - θ * y
-        # @. dj = β * d_ - θ * y
+    minl = typemax(T)
+    j_opt = 0
+    for l=1:K
+        # We want to first fix ``l`` in and compute the maximum with respect to ``j``.
+        # Fixing ``l`` requires calculating a preliminary direction `dl`:
+        
+        ## unpack gradient and compute coefficients
+        gl = DfxT[:, l]
+        β = gl'y
+        θ = gl'd_
+        ## set direction to correspond to ``β(l)dₖ₋₁ + θ(l)yₖ``
+        @. dl = β * d_ - θ * y
 
-        φmin = typemax(T)
-        for gℓ=eachcol(DfxT)
-            _φ = gℓ'dj
-            if _φ <= φmin
-                φmin = _φ
-            end
-        end
+        ## we compute the maximum in simple for loop
+        maxj, j = findmax(gj'dl/ω for gj=eachcol(DfxT))
+        ## adjust for offset ``-⟨gₗ,δₖ⟩`` here
+        maxj -= gl'd
 
-        if φmax <= φmin
-            φmax = φmin
-            dtmp .= dj
+        if maxj <= minl
+            minl = maxj
+            ## minimax has changed, set coefficients for final direction
+            j_opt = j
         end
     end
+    
+    # compute CG direction from optimal coefficients:
+    gj = DfxT[:, j_opt]
+    β = gj'y / ω
+    θ = gj'd_ / ω
+    ## use `dl` as a temporary array
+    dl .= β*d_ - θ*y
+
     y .= d     # set `y` to current steepest descent direction for next iteration
-    d .= dtmp  # set `d` to conjugate gradient direction
-    # dc.phi[] = abs(maximum(d'DfxT))
-    dc.phi[] = abs(φmax)
+    # now, finally set CG direction 
+    d .+= dl
+
+    #dc.phi[] = abs(maximum(d'DfxT))
+    dc.phi[] = minl
 
     if dc.criticality_measure == :sd
         dc.criticality[] = dc.sd_norm_squared[]
